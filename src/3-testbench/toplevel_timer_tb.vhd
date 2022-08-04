e68e0ba32cda21b82a310299915184a4721009c3
nael <naelblog@gmail.com>
2022-08-04 21:45:20 +0200

    more checks in testbench

--- src/3-testbench/toplevel_timer_tb.vhd
+++ src/3-testbench/toplevel_timer_tb.vhd
@@ -7,11 +7,13 @@ end;
 
 architecture tb of toplevel_timer_tb is
  -- constants
+  constant TOTAL_TEST_TIME                   : time    := 500 us;
   constant CYCLES_FROM_TRIGGER_TO_SET_OUTPUT : integer := 10;
-  constant CYCLE_PERIOD : time := 20 ns;
-  constant MIN_DELTA : time := 1 ns;
-  constant BUTTON_LOW_TIME : time := CYCLE_PERIOD * 2.5;
-  constant BUTTON_HIGH_TIME : time := CYCLE_PERIOD * CYCLES_FROM_TRIGGER_TO_SET_OUTPUT * 2; -- twice the needed time to expect the timer output enable
+  constant CYCLE_PERIOD                      : time    := 20 ns;
+  constant TIMER_TIME_FOR_TRIGGER            : time    := CYCLE_PERIOD * CYCLES_FROM_TRIGGER_TO_SET_OUTPUT;
+  constant MIN_DELTA                         : time    := 1 ns;
+  constant BUTTON_LOW_TIME                   : time    := CYCLE_PERIOD * 2.5;
+  constant BUTTON_HIGH_TIME                  : time    := TIMER_TIME_FOR_TRIGGER * 2; -- twice the needed time to expect the timer output enable
  -- signals for inputs
   signal sClock50Mhz    : std_logic := '0';
   signal sButtonTimerEnabled : std_logic := '1';
@@ -20,6 +22,7 @@ architecture tb of toplevel_timer_tb is
  -- signals for process
   signal sSimulationDone : boolean := false;
 
+  constant cExpectedTimeForHigh : time := BUTTON_HIGH_TIME - (TIMER_TIME_FOR_TRIGGER + CYCLE_PERIOD); -- expected time for the output to be 1 (until high impedance)
 begin
 -----------------------------------
 -- instantiate device under test
@@ -31,16 +34,18 @@ begin
 
 -----------------------------------
 -- generate input signals
- -- boolean to terminate simulation after 3 ms
-  sSimulationDone <= false, true after 3 ms;
+ -- boolean to set end for simulation
+  sSimulationDone <= false, true after TOTAL_TEST_TIME;
  -- 50 MHz clock
   sClock50Mhz <= not sClock50Mhz after CYCLE_PERIOD / 2 when not sSimulationDone;
  -- process for button simulation: duty cycle = BUTTON_HIGH_TIME / BUTTON_LOW_TIME
   generatePressedButton : process 
   begin
     if not sSimulationDone then
-      sButtonTimerEnabled <= '0', '1' after BUTTON_LOW_TIME; -- 0 for BUTTON_LOW_TIME, then 1
-      wait for BUTTON_HIGH_TIME;                             -- keeps 1 for BUTTON_HIGH_TIME
+      sButtonTimerEnabled <= '0';
+      wait for BUTTON_LOW_TIME;
+      sButtonTimerEnabled <= '1';
+      wait for BUTTON_HIGH_TIME;
     else
       wait; -- blocks here
     end if;
@@ -70,21 +75,27 @@ begin
 
 -- Triggered timer makes output high
   validateOutputHighAfterTimerDone : process 
-    variable timeSinceLow : time := 0 ns;
+    variable vTimestampLastLow  : time := 0 ns;
+    variable vTimestampLastHigh : time := 0 ns;
   begin
     wait until sOutLed = '0';
-    timeSinceLow := now;
+    vTimestampLastLow := now; -- to check timing in low
     wait until rising_edge(sClock50Mhz);
     wait until sOutLed = '1';
-    -- check that the timer was triggered on time
-    assert (abs((now - timeSinceLow) - (CYCLES_FROM_TRIGGER_TO_SET_OUTPUT * CYCLE_PERIOD)) < MIN_DELTA)
+    vTimestampLastHigh := now; -- to check timing in high
+   -- check that the timer was triggered on time
+    assert (abs((now - vTimestampLastLow) - TIMER_TIME_FOR_TRIGGER) < MIN_DELTA)
       report "Error2 : timer output not 1 after timeout period" severity error;
-    -- check that the output is still 1 after four clocks
+   -- check that the output is still 1 after four clocks
     for counter in 1 to 4 loop
        wait until rising_edge(sClock50Mhz);
        assert (sOutLed = '1')
          report "Error3 : timer output not kept 1 after timeout period" severity error;
      end loop;
+   -- check that is still high until it changes to high impedance
+    wait until sOutLed = 'Z';
+    assert (abs((now - vTimestampLastHigh) - cExpectedTimeForHigh) < MIN_DELTA + (CYCLE_PERIOD / 2)) -- we have half a cycle of incertainty
+      report "Error4 : timer output not 1 long enough" severity error;
   end process;
 
 end tb;
