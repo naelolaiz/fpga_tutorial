a7858c495702c084174a74f72efb8a2760d914d7
naelolaiz <nolaiz@gmail.com>
2022-08-04 22:30:24 +0200

    Working on testbench (#1)
    * updated
    
    * fixed code with updated signal. added new screenshot
    
    Co-authored-by: nael <naelblog@gmail.com>

--- src/3-testbench/toplevel_timer_tb.vhd
+++ src/3-testbench/toplevel_timer_tb.vhd
@@ -7,13 +7,13 @@ end;
 
 architecture tb of toplevel_timer_tb is
  -- constants
-  constant TOTAL_TEST_TIME                   : time    := 500 us;
-  constant CYCLES_FROM_TRIGGER_TO_SET_OUTPUT : integer := 10;
-  constant CYCLE_PERIOD                      : time    := 20 ns;
-  constant TIMER_TIME_FOR_TRIGGER            : time    := CYCLE_PERIOD * CYCLES_FROM_TRIGGER_TO_SET_OUTPUT;
-  constant MIN_DELTA                         : time    := 1 ns;
-  constant BUTTON_LOW_TIME                   : time    := CYCLE_PERIOD * 2.5;
-  constant BUTTON_HIGH_TIME                  : time    := TIMER_TIME_FOR_TRIGGER * 2; -- twice the needed time to expect the timer output enable
+  constant CYCLES_FROM_TRIGGER_TO_SET_OUTPUT  : integer := 10;
+  constant CYCLE_PERIOD                       : time    := 20 ns;
+  constant TIMER_TIME_FOR_TRIGGER             : time    := CYCLE_PERIOD * CYCLES_FROM_TRIGGER_TO_SET_OUTPUT;
+  constant MIN_DELTA                          : time    := 1 ns;
+  constant BUTTON_LOW_TIME                    : time    := CYCLE_PERIOD * 2.5;
+  constant BUTTON_HIGH_TIME_INCLUDING_TRIGGER : time    := TIMER_TIME_FOR_TRIGGER * 2;
+  constant BUTTON_HIGH_TIME_BEFORE_TRIGGER    : time    := TIMER_TIME_FOR_TRIGGER / 2;
  -- signals for inputs
   signal sClock50Mhz    : std_logic := '0';
   signal sButtonTimerEnabled : std_logic := '1';
@@ -33,21 +33,22 @@ begin
 
 -----------------------------------
 -- generate input signals
- -- boolean to set end for simulation
-  sSimulationDone <= false, true after TOTAL_TEST_TIME;
  -- 50 MHz clock
   sClock50Mhz <= not sClock50Mhz after CYCLE_PERIOD / 2 when not sSimulationDone;
- -- process for button simulation: duty cycle = BUTTON_HIGH_TIME / BUTTON_LOW_TIME
+ -- process for button simulation. first from reset to enable and wait until timer enabled, then again with a reset previous to the timer enabled
   generatePressedButton : process 
   begin
-    if not sSimulationDone then
-      sButtonTimerEnabled <= '0';
-      wait for BUTTON_LOW_TIME;
-      sButtonTimerEnabled <= '1';
-      wait for BUTTON_HIGH_TIME;
-    else
-      wait; -- blocks here
-    end if;
+    sButtonTimerEnabled <= '0';
+    wait for BUTTON_LOW_TIME;
+    sButtonTimerEnabled <= '1';
+    wait for BUTTON_HIGH_TIME_INCLUDING_TRIGGER;
+    sButtonTimerEnabled <= '0';
+    wait for BUTTON_LOW_TIME;
+    sButtonTimerEnabled <= '1';
+    wait for BUTTON_HIGH_TIME_BEFORE_TRIGGER;
+    sButtonTimerEnabled <= '0';
+    sSimulationDone <= true;
+    wait; -- blocks here
   end process;
 
 -----------------------------------
@@ -59,7 +60,7 @@ begin
     wait until sButtonTimerEnabled = '0';
     wait for MIN_DELTA; -- wait for the signal to be propagated
     assert (sOutLed = 'Z')
-      report "Error0 : timer output not Z when disabled (reset on)" severity error;
+      report "Timer output not Z when disabled (reset on)" severity error;
   end process;
 
 -- Start timer makes output low
@@ -69,13 +70,13 @@ begin
     wait until rising_edge(sClock50Mhz);
     wait for MIN_DELTA; -- wait for the signal to be propagated
     assert (sOutLed = '0')
-      report "Error1 : timer output not 0 after a fresh reset" severity error;
+      report "Timer output not 0 after a fresh reset" severity error;
   end process;
 
 -- Triggered timer makes output high
   validateOutputHighAfterTimerDone : process
    -- expected time for the output to be 1 (until high impedance)
-    constant cExpectedTimeForHigh : time := BUTTON_HIGH_TIME - (TIMER_TIME_FOR_TRIGGER + CYCLE_PERIOD);
+    constant cExpectedTimeForHighIncludingTrigger : time := BUTTON_HIGH_TIME_INCLUDING_TRIGGER - (TIMER_TIME_FOR_TRIGGER + CYCLE_PERIOD);
    -- used to measure timer LOW time
     variable vTimestampLastLow    : time := 0 ns;
    -- used to measure timer HIGH time
@@ -88,16 +89,16 @@ begin
     vTimestampLastHigh := now; -- to check timing in high
    -- check that the timer was triggered on time
     assert (abs((now - vTimestampLastLow) - TIMER_TIME_FOR_TRIGGER) < MIN_DELTA)
-      report "Error2 : timer output not 1 after timeout period" severity error;
+      report "Timer output not 1 after timeout period" severity error;
    -- check that the output is still 1 after four clocks
     for counter in 1 to 4 loop
        wait until rising_edge(sClock50Mhz);
        assert (sOutLed = '1')
-         report "Error3 : timer output not kept 1 after timeout period" severity error;
+         report "Timer output not kept 1 after timeout period" severity error;
      end loop;
    -- check that is still high until it changes to high impedance
     wait until sOutLed = 'Z';
-    assert (abs((now - vTimestampLastHigh) - cExpectedTimeForHigh) < MIN_DELTA + (CYCLE_PERIOD / 2)) -- we have half a cycle of uncertainty
+    assert (abs((now - vTimestampLastHigh) - cExpectedTimeForHighIncludingTrigger) < MIN_DELTA + (CYCLE_PERIOD / 2)) -- we have half a cycle of uncertainty
       report "Error4 : timer output not 1 long enough" severity error;
   end process;
 
