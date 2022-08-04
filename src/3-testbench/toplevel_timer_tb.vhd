2ede2aacaf376008a1471e94e4b1339152fdf89a
nael <naelblog@gmail.com>
2022-08-04 03:34:03 +0200

    updated

--- src/3-testbench/toplevel_timer_tb.vhd
+++ src/3-testbench/toplevel_timer_tb.vhd
@@ -8,21 +8,23 @@ end;
 architecture tb of toplevel_timer_tb is
  -- signals for inputs
   signal sClock50Mhz    : std_logic := '0';
-  signal sButtonNoReset : std_logic := '1';
+  signal sButtonTimerEnabled : std_logic := '1';
  -- signals for outputs
-  signal sOutLed        : std_logic; 
+  signal sOutNoLed      : std_logic ; 
+  signal sOutLed      : std_logic ; 
 begin
 
 -- instantiate device under tesdt
   DUT : entity work.toplevel_timer(logic)
      generic map (CYCLES_FROM_TRIGGER_TO_SET_OUTPUT => 10)
      port map (inClock50Mhz => sClock50Mhz,
-               inNoReset    => sButtonNoReset, 
-               outNoLed     => sOutLed);
+               inNoReset    => sButtonTimerEnabled, 
+               outNoLed     => sOutNoLed);
 
 -- generate input signals
   sClock50Mhz <= not sClock50Mhz after 10 ns;
-  sButtonNoReset <= not sButtonNoReset after 300 ns;
+  sButtonTimerEnabled <= not sButtonTimerEnabled after 310 ns;
+  sOutLed <= not sOutNoLed;
 
 end tb;
 
