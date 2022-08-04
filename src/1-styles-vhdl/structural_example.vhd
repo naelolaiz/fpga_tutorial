4faabea0891fe00bc980b0de8c95fd6a33161636
nael <naelblog@gmail.com>
2022-08-04 23:15:13 +0200

    reverted structural example

--- src/1-styles-vhdl/structural_example.vhd
+++ src/1-styles-vhdl/structural_example.vhd
@@ -10,15 +10,15 @@ entity structural_example is
  end;
 
  architecture structural of structural_example is
---    component OR2
---       port (IN1, IN2 : in std_logic;
---             OUT1     : inout std_logic);
---    end component;
+    component or_gate
+       port (i1, i2 : in std_logic;
+             o1 : out std_logic);
+    end component;
 
---    component AND2
---       port (IN1, IN2 : in std_logic;
---             OUT1     : inout std_logic);
---    end component;
+    component and_gate
+       port (i1, i2 : in std_logic;
+             o1 : out std_logic);
+    end component;
 
     component not_gate
        port (i1 : in std_logic;
@@ -30,15 +30,15 @@ entity structural_example is
 
  begin
     
-    andInstance : AND2
-    port map(IN1 => not Button1,
-             IN2 => not Button2); --,
---             OUT => sOutAnd);
+    andInstance : and_gate
+    port map(i1 => not Button1,
+             i2 => not Button2,
+             o1 => sOutAnd);
 
-    orInstance : OR2
-    port map(IN1 => sOutAnd,
-             IN2 => not Button3); --,
---             OUT => sLed);
+    orInstance : or_gate
+    port map(i1 => sOutAnd,
+             i2 => not Button3,
+             o1 => sLed);
 
     notInstance : not_gate
     port map (i1 => sLed,
