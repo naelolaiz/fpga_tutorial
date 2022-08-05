95c391a73b1554c7d1d779fdea82818bd827fbdb
nael <naelblog@gmail.com>
2022-08-05 02:08:48 +0200

    fixed test

--- src/1-styles-vhdl/styles_example_tb.vhd
+++ src/1-styles-vhdl/styles_example_tb.vhd
@@ -9,6 +9,7 @@ architecture tb of styles_example_tb is
    signal sInButton1, sInButton2, sInButton3 : std_logic := '1';
    signal sLedBehavioral, sLedDataflow, sLedStructural : std_logic := '1';
    signal sExpectedQ : std_logic := '0';
+   signal sI : integer;
 
     -- declare record type
     type test_vector is record
@@ -21,7 +22,7 @@ architecture tb of styles_example_tb is
         -- a, b, c , q   -- positional method is used below
         ('0', '0', '0', '0'),
         ('0', '0', '1', '0'),
-        ('0', '1', '0', '1'),
+        ('0', '1', '0', '0'),
         ('0', '1', '1', '1'),
         ('1', '0', '0', '0'),
         ('1', '0', '1', '1'),
@@ -50,19 +51,36 @@ begin
     tb : process
     begin
         for i in test_vectors'range loop
+            sI <= i;
             sInButton1 <= test_vectors(i).a;
             sInButton2 <= test_vectors(i).b;
-            sInButton2 <= test_vectors(i).c;
+            sInButton3 <= test_vectors(i).c;
             sExpectedQ <= test_vectors(i).q;
 
-            wait for 20 ns;
+            wait for 10 ns;
 
             assert ( 
                      sLedBehavioral = sLedDataflow
                      and sLedBehavioral = sLedStructural
                      and sLedStructural = sExpectedQ
                     )
-            report  "error detected"
+            report  "error detected! "
+                     & "i: " & integer'image(sI)
+                     & ". Inputs: "
+                     & std_logic'image(sInButton1) 
+                     & ", " 
+                     & std_logic'image(sInButton2) 
+                     & ", " 
+                     & std_logic'image(sInButton3) 
+                     & ". behavioral: " 
+                     & std_logic'image(sLedBehavioral) 
+                     & ", dataflow: " 
+                     & std_logic'image(sLedDataflow)
+                     & ", structural: " 
+                     & std_logic'image(sLedStructural) 
+                     & ", expected: " 
+                     & std_logic'image(sExpectedQ) 
+
                     severity error;
         end loop;
         wait;
